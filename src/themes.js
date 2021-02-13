bf83696caf0b6a238626016a7b85d1b4555437ab
Guilherme de Lyra <guilyra12@gmail.com>
2021-02-12 22:56:46 -0300

    adding basic template for ebook

---
+++ src/themes.js
@@ -0,0 +1,28 @@
+const themes = {
+    light: {
+      colors: {
+        text: '#282230',
+        background: {
+          _: '#f1f1f1',
+          contrast: '#b1b1b1',
+        },
+        primary: '#01796f',
+        primary_dark: '#016159',
+        secondary: '#562931',
+      },
+    },
+    dark: {
+        colors: {
+        text: '#f1f1f1',
+        background: {
+            _: '#27323a',
+            contrast: '#0d1215',
+        },
+        primary: '#01978b',
+        primary_dark: '#00887c',
+        secondary: '#fe8690',
+        },
+    },
+}
+
+export default themes;
