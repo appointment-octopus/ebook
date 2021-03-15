e96ad2bdfd6125bdd1ad25e465291634d022c1be
Guilherme de Lyra <guilyra12@gmail.com>
2021-03-15 16:42:19 -0300

    removing cache from ci since it wasnt working

--- tools/generate_summary.py
+++ tools/generate_summary.py
@@ -1,16 +1,22 @@
 import glob
 
 result = '# Summary\n\n'
+folders = set()
 for f in glob.glob("src/**/*.md", recursive=True):
-    if 'SUMMARY' not in f:
+    if 'SUMMARY' not in f and 'README' not in f:
         tokens = f.split('/')
         filename = tokens[-1]
-        name = (filename.strip('.md') if filename != 'README.md' else tokens[-2]).title()
-        path = f.split('src/')[-1].replace(' ', '%20')
+        name = filename.strip('.md').title()
+        path = f.split('src/')[-1]
+        directory = '/'.join(path.split('/')[:-1])
         indent = (len(tokens)-2)*2
-        if 'README' in f:
-            indent -= 2
-        result += f'{" "*indent}- [{name}](./{path})\n'
+
+        if directory and directory not in folders:
+            dir_name = directory.split('/')[-1].title()
+            result += f'{" "*(indent-2)}- [{dir_name}](./{directory.replace(" ", "%20")}/README.md)\n'            
+            folders.add(directory)
+
+        result += f'{" "*indent}- [{name}](./{path.replace(" ", "%20")})\n'
 
 with open('src/SUMMARY.md', 'w') as file:
     file.write(result)
